173fdc849eda7c69361e33e631ba5a0426f6a30a
Eckintosh <4ai360@gmail.com>
2026-09-16 12:58:07 +0000

    Add MongoDB database connection config

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI environment variable is missing or undefined.");
+        }
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("Error Connecting to MongoDB:", error.message || error);
+    }
+}
+
+module.exports = connectDB;
